f9b626cc442b08aa2ec527e4ff66928d109d2b33
peachtuntiwong <peach.tuntiwong@gmail.com>
2017-02-17 00:18:06 +1100

    day4 hw & minor changes on other hw

--- peach_tuntiwong/week01/day3/js/main.js
+++ peach_tuntiwong/week01/day3/js/main.js
@@ -3,24 +3,37 @@ var currentGuess = ['_', '_', '_'];
 
 var checkLetter = function(letter) {
   var inputLetter = letter.toUpperCase(); // Set all input to uppercase
-  if (inputLetter == 'F') { // Return 1 if guess correct, return 0 if guess wrong
-    currentGuess[0] = inputLetter;
-    return 1;
-  } else if (inputLetter == 'O') {
-    currentGuess[1] = inputLetter;
-    return 1;
-  } else if (inputLetter == 'X') {
-    currentGuess[2] = inputLetter;
-    return 1;
-  } else {
-    return 0;
-  }
-}
+  var guessFlag = false;
+
+  // for (var i = 0; i < solution.length; i++) {
+  //   if (inputLetter === solution[i]){
+  //     guessFlag = true;
+  //     currentGuess[i] = solution[i];
+  //   }
+  // }
+
+  // for (i in solution) {
+  //   if (inputLetter === solution[i]){
+  //     guessFlag = true;
+  //     currentGuess[i] = solution[i];
+  //   }
+  // }
+
+  solution.forEach(function(correctLetter, i){
+    if (inputLetter === correctLetter){
+      guessFlag = true;
+      currentGuess[i] = correctLetter;
+      console.log(currentGuess);
+    }
+  });
+
+  return guessFlag;
+};
 
 var guessLetter = function(letter) {
   checkLetter(letter);
   console.log(currentGuess);
-  if (currentGuess.indexOf('_') == -1) { // Check for winning condition
+  if (currentGuess.indexOf('_') === -1) { // Check for winning condition
     console.log('Congratulation, you won!');
   }
 }
@@ -34,7 +47,7 @@ var wheelOfFortune = function(letter) {
     reward--;
   }
   console.log(currentGuess);
-  if (currentGuess.indexOf('_') == -1) { // Check for winning condition
+  if (currentGuess.indexOf('_') === -1) { // Check for winning condition
     console.log('Congratulation, you won $' + reward + '!');
   }
 }
@@ -45,10 +58,10 @@ var hangman = function(letter) {
   if (!checkLetter(letter)) { // If guess wrongly, counter goes up
     counter++;
   }
-  if (counter == 6) { // Check for losing condition
+  if (counter === 6) { // Check for losing condition
     console.log('You lose...');
   }
-  if (currentGuess.indexOf('_') == -1) { //Check for winning condition
+  if (currentGuess.indexOf('_') === -1) { //Check for winning condition
     console.log('Congratulation, you won!');
   }
 }
@@ -77,7 +90,7 @@ var maxOfThree = function(num1, num2, num3) {
 
 var checkVovel = function(letter) {
   var input = letter.toUpperCase();
-  if (input == 'A' || input == 'E' || input == 'I' || input == 'O' || input == 'U') {
+  if (input === 'A' || input === 'E' || input === 'I' || input === 'O' || input === 'U') {
     return true;
   } else {
     return false;
@@ -86,7 +99,7 @@ var checkVovel = function(letter) {
 
 var sumArray = function(numbers) {
   var sum = 0;
-  for (var i = 0; i < numbers.length; i++) {
+  for (i in numbers) {
     sum += numbers[i];
   }
   return sum
@@ -94,7 +107,7 @@ var sumArray = function(numbers) {
 
 var multiplyArray = function(numbers) {
   var sum = 1;
-  for (var i = 0; i < numbers.length; i++) {
+  for (i in numbers) {
     sum = sum * numbers[i];
   }
   return sum
---
+++ peach_tuntiwong/week01/day4/js/main.js
@@ -0,0 +1,142 @@
+// Cash Register
+
+var cartForParty = {
+  banana: "1.25",
+  handkerchief: ".99",
+  Tshirt: "25.01",
+  apple: "0.60",
+  nalgene: "10.34",
+  proteinShake: "22.36"
+};
+
+var cashRegister = function(cart) {
+  var sum = 0; // Initilize sum of price
+  var nameList = Object.keys(cart); // Get list of property names, so we can use to get the price
+  nameList.forEach(function(item){
+    sum += parseFloat(cart[item]); // Actual calculation
+  })
+  return sum;
+}
+
+
+//----------------------------------------------------------------------------
+
+// Credit Card Validation
+// Initialize errorMessage message array
+var errorMessage = [];
+
+
+// Cut remove '-' from card number; return in sections of 4
+var cutSection = function(cardNum, section) {
+  switch (section) {
+    case 1:
+      return cardNum.slice(0,4);
+      break;
+    case 2:
+      return cardNum.slice(5,9);
+      break;
+    case 3:
+      return cardNum.slice(10,14);
+      break;
+    case 4:
+      return cardNum.slice(15,19);
+      break;
+  }
+}
+
+// Combind card number and convert to integer
+var getCardNum = function(cardNum) {
+  cardStr = cutSection(cardNum, 1) + cutSection(cardNum, 2) + cutSection(cardNum, 3) + cutSection(cardNum, 4);
+  return parseInt(cardStr);
+}
+
+// Make sure that card number is integer
+var checkInputType = function(cardInput) {
+  if (Number.isInteger(cardInput)) {
+    return cardInput;
+  } else {
+    return getCardNum(cardInput);
+  }
+}
+
+// Check card length
+var checkCardLength = function(num) {
+  if (String(num).length === 16) {
+    return true;
+  } else {
+    errorMessage.push('Wrong_length');
+  }
+}
+
+// Card validity conditions
+// Check different digits
+var isAtLeast2digits = function(num) {
+  var str = String(num);
+  for (i in str) {
+    if (str[0] !== str[i]) {
+      return true;
+    }
+  }
+  errorMessage.push('Single_digit');
+  return false;
+}
+
+// Check if the last digit is even
+var isEven = function(num) {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    errorMessage.push('Odd_card_number');
+  }
+}
+
+// Sum card number
+var sumCardNumber = function(num) {
+  var sum = 0;
+  var str = String(num);
+  for (i in str) {
+    sum += parseInt(str[i]);
+  }
+  return sum;
+}
+
+// Check sum of card number
+var checkSum = function (num) {
+  if (sumCardNumber(num)>16) {
+    return true;
+  } else {
+    errorMessage.push('Sum_less_than_16');
+    return false;
+  }
+}
+
+// validate card number
+var validateCreditCard = function(input) {
+  var validCard = new Object;
+  var valid = false;
+  var cardNum = checkInputType(input); //return as integer
+
+  //list of requirements:
+  //16 digits numbers
+  //(String(cardNum).length === 16)
+
+  //at least 2 different digits
+  //isAtLeast2digits(cardNum)
+
+  //final digit must be even
+  //cardNum % 2 == 0
+
+  //sum of all digits > 16
+  //sumCardNumber(cardNum)
+
+  if (checkCardLength(cardNum) && isAtLeast2digits(cardNum) && isEven(cardNum) && checkSum(cardNum)) { // Check conditions
+    validCard.valid = true;
+    validCard.number = input;
+  } else {
+    validCard.valid = false;
+    validCard.number = input;
+    validCard.error = errorMessage;
+  }
+
+  return validCard;
+}
