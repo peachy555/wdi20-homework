e0b73c126084752daff0cd57108e1d74d7915897
peachtuntiwong <peach.tuntiwong@gmail.com>
2017-02-18 13:09:13 +1100

    running code with user interface; additional test combinations

--- peach_tuntiwong/week01/day5/js/main.js
+++ peach_tuntiwong/week01/day5/js/main.js
@@ -6,14 +6,14 @@
 
 // ************* Trip Planning ******************
 
+// Database
 var subway = {
   N: ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'],
   L: ['8th', '6th', 'Union Square', '3rd', '1st'],
   6: ['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place']
 };
 
-// ************** Calculations
-// Get index (location) of single stop from database
+// Return index (location) of single stop from database
 var getIndex = function(fromStop, toStop, line) { // arguements: string, string, array
   var index = {
     fromIndex: line.indexOf(fromStop),
@@ -22,7 +22,7 @@ var getIndex = function(fromStop, toStop, line) { // arguements: string, string,
   return index;
 }
 
-// Get distance and direction from A to B on single line
+// Return distance and direction from A to B on single line
 var getDistance = function(index) {
   var tripProperties = { // Initialize as going forward
     distance: index.toIndex - index.fromIndex,
@@ -36,11 +36,11 @@ var getDistance = function(index) {
   return tripProperties;
 }
 
-// Get section of trip for printout
+// Return section of trip for printout
 var getSection = function(index, line, reverse) {
   // Check for reverse direction
   if (!reverse) {
-    return line.slice(index.fromIndex+1, index.toIndex+1);
+    return line.slice(index.fromIndex + 1, index.toIndex + 1);
   } else {
     return line.slice(index.toIndex, index.fromIndex).reverse();
   }
@@ -80,6 +80,7 @@ var planTripSingle = function(fromLine, fromStop, toLine, toStop) {
 }
 
 var planTripCross = function(fromLine, fromStop, toLine, toStop) {
+  // First section
   var index = getIndex(fromStop, 'Union Square', subway[fromLine]); // Do it this way, so I can use index for getDistance() and getSection()
   var fromTripData = getDistance(index); // Get distance from fromStop to toStop
   var fromSection = getSection(index, subway[fromLine], fromTripData.reverse);// Get section for printResult()
@@ -96,9 +97,6 @@ var planTripCross = function(fromLine, fromStop, toLine, toStop) {
 // ********** Main function
 // Decide single line or cross over
 var planTrip = function(fromLine, fromStop, toLine, toStop) {
-  // ************************************************************************
-  // Main function(fromStop, fromLine, toStop, toLine)
-  lineIndex = Object.keys(subway);
   if (fromLine === toLine) {
     // Case: on the same line
     planTripSingle(fromLine, fromStop, toLine, toStop);
@@ -109,52 +107,77 @@ var planTrip = function(fromLine, fromStop, toLine, toStop) {
   }
 }
 
-// *******************Test Code
-// Cross Over, with reverse
-// planTrip('N', '23rd', '6', 'Grand Central');
-
 
 // *************** User Interface ******************
 
+// *************** Input checks
 // Check if user inputs are valid
-// var checkLine = function(fromLine, toLine) {
-//   if((Object.keys(subway).indexOf(fromLine) !== -1) && (Object.keys(subway).indexOf(toLine) !== -1)) {
-//     return { // Pass
-//       passFlag: 1,
-//       fromLine: fromLine,
-//       toLine: toLine
-//     };
-//   } else { // Fail
-//     return {
-//       passFlag: 0,
-//       fromLine: 'Fail checkLine',
-//       toLine: 'Fail checkLine'
-//     };
-//   }
-// }
-//
-// var checkStation = function(passOn, fromStop, toStop) {
-//   if (passOn.passFlag) { // Pass checkLine, have to check stops
-//     if(1) {
-//     } else {
-//       console.log('fail bypass');
-//       return 0;
-//     }
-//   }
-// }
-// console.log(checkLine('6', 'k'), 'fromStop', 'toStop');
-//
-// var userInterface = function(){
-//   var fromStop = prompt("Please enter your starting stop.", "Station name");
-//   var fromLine = prompt("Please enter your starting line.", "Line name");
-//   var toStop = prompt("Please enter your destination stop.", "Station name");
-//   var toLine = prompt("Please enter your destination's line.", "Line name");
-//
-//   if (checkStation(checkLine(fromLine, toLine), fromStop, toStop)) {
-//     planTrip(fromLine, fromStop, toLine, toStop);
-//   }
-// }
+var checkLine = function(line) {
+  if(Object.keys(subway).indexOf(line) !== -1) {
+    return 1;
+  } else {
+    console.log('Invalid line name. Please try again.');
+    return 0;
+  }
+}
 
-// userInterface();
+var checkStop = function(stop, line) {
+  if (subway[line].indexOf(stop) !== -1) {
+    return 1;
+  } else {
+    console.log('Invalid stop name. Please try again');
+    return 0;
+  }
+}
 
-// console.log(planTrip('6', 'Grand Central', '6', '23rd'));
+var getLineList = function() {
+  return Object.keys(subway);
+}
+
+// ******** Main function
+var userInterface = function(){
+  var lineList = getLineList();
+
+  var fromLine = prompt("Please enter your starting line.\nLine: " + lineList, "Line name");
+  if (!checkLine(fromLine)) {
+    return 0;
+  }
+
+  var fromStop = prompt("Please enter your starting stop.\nLine: " + fromLine + "\nStop: " + subway[fromLine], "Station name");
+  if (!checkStop(fromStop, fromLine)) {
+    return 0;
+  }
+
+  var toLine = prompt("Please enter your destination's line.\nLine: " + lineList, "Line name");
+  if (!checkLine(toLine)) {
+    return 0;
+  }
+
+  var toStop = prompt("Please enter your destination stop.\nLine: " + toLine + "\nStop: " + subway[toLine], "Station name");
+  if (!checkStop(toStop, toLine)) {
+    return 0;
+  }
+
+  planTrip(fromLine, fromStop, toLine, toStop);
+}
+
+userInterface();
+
+// ************** Test Combinations
+// Single line, forward
+// planTrip('N', '23rd', 'N', 'Grand Central');
+
+
+// Single line, backward
+// planTrip('L', '1st', 'L', '6th');
+
+
+// Cross Over, forward only
+// planTrip('N', 'Time Squares', 'L', '3rd');
+
+
+// Cross Over, with reverse
+// planTrip('N', '23rd', '6', 'Grand Central');
+
+// Cross Over, same station name
+// planTrip('N', '28th', '6', '28th');
