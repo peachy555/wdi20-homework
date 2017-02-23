f8aedcf1f1c5610841f3b70a358cc789cb4d2791
peachtuntiwong <peach.tuntiwong@gmail.com>
2017-02-23 14:24:55 +1100

    day08 simple cat walk forward/backward + flip in single function

--- peach_tuntiwong/week02/day08/js/main.js
+++ peach_tuntiwong/week02/day08/js/main.js
@@ -1,10 +1,43 @@
 
 
 var catImg = document.getElementById("cat");
-var catPos = 0;
-var catWalk = function() {
-  catPos += 1;
+var catPos = 0; //current desired position of image
+var timeInterval = 5; //set speed
+var isForward = 1; //use to indicate direction and index for shifting image
+catImg.style.top = (window.innerHeight-catImg.height)/2 + "px";
+
+
+var catFlip = function() {
+  if (isForward === 1) {
+    catImg.style.transform = "scaleX(-1)";
+    isForward = -1;
+  } else {
+    catImg.style.transform = "scaleX(1)";
+    isForward = 1;
+  }
+};
+
+var moveCat = function() {
+  catPos += isForward;
   catImg.style.left = catPos + "px";
 }
 
-setInterval(catWalk, 15);
+var catWalk = function() {
+  var catIntervalForward = setInterval(function() {
+    if (isForward === 1) {
+      console.log(isForward);
+      moveCat();
+      if (catPos === (window.innerWidth-catImg.width)) {
+        catFlip();
+      }
+    } else {
+      moveCat();
+      if (catPos === 0) {
+        catFlip();
+      }
+    }
+
+  }, timeInterval);
+};
+
+catWalk();
