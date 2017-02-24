e8fdd23e761a5d78180ae2628a348c72c9351d46
peachtuntiwong <peach.tuntiwong@gmail.com>
2017-02-24 11:23:54 +1100

    no big updates

--- peach_tuntiwong/week02/day08/js/main.js
+++ peach_tuntiwong/week02/day08/js/main.js
@@ -1,43 +1,66 @@
+$(document).ready(function(){
 
+  var catImg = $("#cat");
+  var catPos = 0; //current desired position of image
+  var timeInterval = 50; //set speed
+  var isForward = 1; //use to indicate direction and index for shifting image
+  catImg.css('top', ((window.innerHeight-catImg.height())/2 + "px"));
 
-var catImg = document.getElementById("cat");
-var catPos = 0; //current desired position of image
-var timeInterval = 5; //set speed
-var isForward = 1; //use to indicate direction and index for shifting image
-catImg.style.top = (window.innerHeight-catImg.height)/2 + "px";
-
-
-var catFlip = function() {
-  if (isForward === 1) {
-    catImg.style.transform = "scaleX(-1)";
-    isForward = -1;
-  } else {
-    catImg.style.transform = "scaleX(1)";
-    isForward = 1;
+  var newBackgroundColor = function() {
+    document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
   }
-};
 
-var moveCat = function() {
-  catPos += isForward;
-  catImg.style.left = catPos + "px";
-}
-
-var catWalk = function() {
-  var catIntervalForward = setInterval(function() {
+  var catFlip = function() {
     if (isForward === 1) {
-      console.log(isForward);
-      moveCat();
-      if (catPos === (window.innerWidth-catImg.width)) {
-        catFlip();
-      }
+      catImg.css("transform", "scaleX(-1)");
+      isForward = -1;
     } else {
-      moveCat();
-      if (catPos === 0) {
-        catFlip();
-      }
+      catImg.css("transform", "scaleX(1)");
+      isForward = 1;
     }
+  };
 
-  }, timeInterval);
-};
+  var moveCat = function() {
+    catPos += isForward;
+    catImg.css("left", catPos + "px");
+  }
 
-catWalk();
+  var checkReachCenter = function() {
+    if (catPos == Math.floor((window.innerWidth-catImg.width())/2)) {
+      console.log("reach center");
+    }
+  }
+
+  var catWalk = function() {
+    var catIntervalForward = setInterval(function() {
+      checkReachCenter();
+      // newBackgroundColor();
+
+      if (isForward === 1) {
+        moveCat();
+        if (catPos === (window.innerWidth-catImg.width())) {
+          catFlip();
+        }
+      } else {
+        moveCat();
+        if (catPos === 0) {
+          catFlip();
+        }
+      }
+
+    }, timeInterval);
+  };
+
+  catWalk();
+
+
+
+
+
+
+
+
+
+
+
+})
