bb2eaa99b2d889b0c46061d451946da1861ce54f
peachtuntiwong <peach.tuntiwong@gmail.com>
2017-02-24 15:01:57 +1100

    minor update on day07

--- peach_tuntiwong/week02/day08/js/main.js
+++ peach_tuntiwong/week02/day08/js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 
-  var catImg = $("#cat");
+  var $catImg = $("#cat");
   var catPos = 0; //current desired position of image
-  var timeInterval = 50; //set speed
+  var timeInterval = 5; //set cat speed
+  var interuptInterval = 30;
   var isForward = 1; //use to indicate direction and index for shifting image
-  catImg.css('top', ((window.innerHeight-catImg.height())/2 + "px"));
+  $catImg.css('top', ((window.innerHeight-$catImg.height())/2 + "px"));
 
   var newBackgroundColor = function() {
     document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
@@ -12,33 +13,63 @@ $(document).ready(function(){
 
   var catFlip = function() {
     if (isForward === 1) {
-      catImg.css("transform", "scaleX(-1)");
+
       isForward = -1;
+      $catImg.css("transform", "scaleX(" + isForward + ")");
     } else {
-      catImg.css("transform", "scaleX(1)");
       isForward = 1;
+      $catImg.css("transform", "scaleX(" + isForward + ")");
     }
   };
 
   var moveCat = function() {
     catPos += isForward;
-    catImg.css("left", catPos + "px");
+    $catImg.css("left", catPos + "px");
   }
 
-  var checkReachCenter = function() {
-    if (catPos == Math.floor((window.innerWidth-catImg.width())/2)) {
-      console.log("reach center");
+  var isMidWay = function() {
+    if (catPos == Math.floor((window.innerWidth-$catImg.width())/2)) {
+      return true;
     }
   }
 
-  var catWalk = function() {
-    var catIntervalForward = setInterval(function() {
-      checkReachCenter();
-      // newBackgroundColor();
 
+  var runInterupt = function() {
+    // Now blank page
+    var catInterupt = setInterval(function() {
+
+
+    }, interuptInterval);
+    // end of interupt
+    hideInterupt();
+  }
+
+  var hideInterupt = function () {
+
+  }
+
+
+  var backToCat = function() {
+    // Now blank page
+
+  }
+
+  var catWalk = function() {
+    var catInterval = setInterval(function() {
+
+      //check position
+      if (isMidWay()) {
+        clearInterval(catInterval);
+        $catImg.hide();
+        runInterupt();
+        backToCat();
+      }
+
+
+      // normal cat walking
       if (isForward === 1) {
         moveCat();
-        if (catPos === (window.innerWidth-catImg.width())) {
+        if (catPos === (window.innerWidth-$catImg.width())) {
           catFlip();
         }
       } else {
